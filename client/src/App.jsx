cb8c551ed064a74926d254d8688c5011c232ca63
deNeedo <jkudrys@protonmail.com>
2024-11-28 01:27:12 +0100

    Modified app logic to avoid error 429 when making terrain profile and styling changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,6 @@ import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import About from "./components/About.jsx";
 import Home from "./components/Home.jsx";
 import Login from "./components/Login.jsx"
-import Logout from "./components/Logout.jsx";
-import MapView from "./components/MapView.jsx";
 import Register from "./components/Register.jsx"
 import ServerRequest from "./components/ServerRequest.jsx";
 import Verify from "./components/Verify.jsx";
@@ -17,25 +15,27 @@ const App = () => {
             <div className={Styles.container}>
                 <div className={Styles.pageContent}>
                     <Routes>
-                        <Route path="/" element={<Home />} />
+                        <Route path="/" element={<RouteSelector children={[<Home key={0}/>, <Login key={1}/>]} />} />
                         <Route path="/about" element={<About />} />
                         <Route path="/login" element={<RouteSelector children={[<Home key={0}/>, <Login key={1}/>]} />} />
-                        <Route path="/logout" element={<RouteSelector children={[<Logout key={0}/>, <Login key={1}/>]} />} />
-                        <Route path="/map" element={<RouteSelector children={[<MapView key={0}/>, <Login key={1}/>]} />} />
                         <Route path="/register" element={<RouteSelector children={[<Home key={0}/>, <Register key={1}/>]} />} />
                         <Route path="/verify" element={<RouteSelector children={[<Home key={0}/>, <Verify key={1}/>]} />} />
                         <Route path="*" element={<ServerRequest />} />
                     </Routes>
                 </div>
                 <div className={Styles.bottomMenu}>
-                    <ul>
-                        <li> <Link to="/"> Home </Link> </li>
-                        <li> <Link to="/login"> Login </Link> </li>
-                        <li> <Link to="/logout"> Logout </Link> </li>
-                        <li> <Link to="/register"> Register </Link> </li>
-                        <li> <Link to="/about"> About </Link> </li>
-                        <li> <Link to="/map"> Map View </Link> </li>
-                    </ul>
+                    <button className={Styles.bottomMenuButton}>
+                        <Link to="/"> Home </Link>
+                    </button>
+                    <button className={Styles.bottomMenuButton}>
+                        <Link to="/login"> Login </Link>
+                    </button>
+                    <button className={Styles.bottomMenuButton}>
+                        <Link to="/register"> Register </Link>
+                    </button>
+                    <button className={Styles.bottomMenuButton}>
+                        <Link to="/about"> About </Link>
+                    </button>
                 </div>
             </div>
         </BrowserRouter>
--- server/src/main/java/org/deneedo/geomap/controllers/CollectionController.java
+++ server/src/main/java/org/deneedo/geomap/controllers/CollectionController.java
@@ -10,7 +10,6 @@ import org.deneedo.geomap.repositories.MarkerRepository;
 import org.deneedo.geomap.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,7 +35,7 @@ public class CollectionController {
         return "Markers received successfully!";
     }
 
-    @GetMapping("/loadCollection")
+    @PostMapping("/loadCollection")
     public List<Marker> loadCollection(@RequestHeader Map<String, String> headers) {
         User foundUser = userRepository.findByVerificationToken(headers.get("auth-token"));
         String username = foundUser.getUsername();
